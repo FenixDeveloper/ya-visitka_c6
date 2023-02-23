bd5b05f9bd47e44acb615e911afafb746c0f25ad
Alexander Afanasiev <armawir1@mail.ru>
2023-02-23 14:53:41 +0700

    change package.json

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import "./fonts/fonts.css";
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
