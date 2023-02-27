50460195d4f55b0e59bb1222e87b29c76197a238
Mikhail Nenashev <nord-vizjerei@yandex.ru>
2023-02-27 19:05:10 +0200

    added comment deletion

--- backend/src/routes/comments.ts
+++ backend/src/routes/comments.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getComments } from '../controllers/comments';
+import { deleteComment, getComments } from '../controllers/comments';
+import { isCommentIdValid } from '../validators/comment';
+import curatorGuard from '../middlewares/curatorGuard';
 
 const router = Router();
 
+router.use(curatorGuard);
 router.get('/', getComments);
+router.delete('/:commentId', isCommentIdValid, deleteComment);
 
 export default router;
