10bb700d36c3335b24e4a73567ab93027347050a
Rita Rixter <ritarixter@mail.ru>
2023-02-28 18:53:03 +0300

    Добавила сортировку по городам, окно с коммент по клику, переход на визизтку по клику на фото

--- frontend/src/components/ProfileCard/ProfileCard.tsx
+++ frontend/src/components/ProfileCard/ProfileCard.tsx
@@ -1,18 +1,56 @@
-import { FC } from "react";
-import styles from "./ProfileCard.module.css";
-import { IProfileCard } from "../../utils/types";
-import icon from "../../images/icons/comment.svg";
+import { FC, useState } from 'react';
+import styles from './ProfileCard.module.css';
+import { IProfileCard } from '../../utils/types';
+import icon from '../../images/icons/comment.svg';
+import CommentPost from '../comment-post/comment-post';
+import { Link } from 'react-router-dom';
 
 const ProfileCard: FC<IProfileCard> = (props) => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <li className={styles.card}>
-      <img src={props.image} className={styles.image} alt="Фото человека" />
-      {props.comments_number !== 0 && (
-        <div className={styles.comments_number}>{props.comments_number}</div>
+      <Link className={styles.link} to={'/vizitka'}>
+      <img
+        src={props.image}
+        className={!open ? styles.image : styles.image_open}
+        alt="Фото человека"
+      />
+            </Link>
+      <div
+      className={styles.icon__comment}
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {props.comments_number !== 0 && (
+          <div
+            className={
+              open ? styles.comments_number_open : styles.comments_number
+           }
+          >
+            {props.comments_number}
+          </div>
+        )}
+        <img src={icon} className={open ? styles.icon_open : styles.icon} alt="Иконка комментариев" />
+      </div>
+      {open && (
+        <div className={styles.comment}>
+          <CommentPost
+            comments={['Классная фотка', 'Очень инетерсно', 'Комментарий 1', 'Комментарий 2', 'Комментарий 3', 'Комментарий 4', 'Комментарий 4', 'Комментарий 4' ,'Комментарий 4' ,'Комментарий 4']}
+            emojies={[{ type: '', count: 3 }]}
+            class={true}
+          />
+        </div>
       )}
-      <img src={icon} className={styles.icon} alt="Иконка комментариев" />
-      <p className={styles.name}>{props.name}</p>
+      <p
+        className={`${styles.name} ${
+          !open ? styles.name_close : styles.name_open
+        }`}
+      >
+        {props.name}
+      </p>
       <p className={styles.city}>{props.city}</p>
+
     </li>
   );
 };
--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,69 +1,84 @@
-import styles from "./MainPage.module.css";
-import ProfileCard from "../../components/ProfileCard/ProfileCard";
-import person1 from "./imagesData/person_1.png";
-import person2 from "./imagesData/person_2.png";
-import person3 from "./imagesData/person_3.png";
-import person4 from "./imagesData/person_4.png";
-import DropdownList from "../../components/DropdownList/DropdownList";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-const props = [
+import styles from './MainPage.module.css';
+import ProfileCard from '../../components/ProfileCard/ProfileCard';
+import person1 from './imagesData/person_1.png';
+import person2 from './imagesData/person_2.png';
+import person3 from './imagesData/person_3.png';
+import person4 from './imagesData/person_4.png';
+import DropdownList from '../../components/DropdownList/DropdownList';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+interface IData {
+  name: string;
+  city: string;
+  image: string;
+}
+const data: Array<IData> = [
   //тестовые данные
   {
-    name: "Иванов Сергей",
-    city: "Москва",
+    name: 'Иванов Сергей',
+    city: 'Москва',
     image: person1,
   },
   {
-    name: "Степанов Дмитрий",
-    city: "Жемчужное Костромской обл",
+    name: 'Степанов Дмитрий',
+    city: 'Жемчужное Костромской обл',
     image: person2,
   },
   {
-    name: "Наиля Назимова",
-    city: "Кингисепп Ленинградской обл",
+    name: 'Наиля Назимова',
+    city: 'Кингисепп Ленинградской обл',
     image: person3,
   },
   {
-    name: "Виктория Листвина",
-    city: "Калуга",
+    name: 'Виктория Листвина',
+    city: 'Калуга',
     image: person4,
   },
   {
-    name: "Иванов Сергей",
-    city: "Москва",
+    name: 'Иванов Сергей',
+    city: 'Москва',
     image: person1,
   },
   {
-    name: "Степанов Дмитрий",
-    city: "Жемчужное Костромской обл",
+    name: 'Степанов Дмитрий',
+    city: 'Жемчужное Костромской обл',
     image: person2,
   },
   {
-    name: "Наиля Назимова",
-    city: "Кингисепп Ленинградской обл",
+    name: 'Наиля Назимова',
+    city: 'Кингисепп Ленинградской обл',
     image: person3,
   },
   {
-    name: "Виктория Листвина",
-    city: "Калуга",
+    name: 'Виктория Листвина',
+    city: 'Калуга',
     image: person4,
   },
 ];
 
 export const MainPage = (props1: any) => {
-  const cities: Array<string> = [];
-  props.forEach((item) => {
+  const [city, setCity] = useState<string>('Все города');
+  const [props, setProps] = useState<Array<IData>>(data);
+  const cities: Array<string> = ['Все города'];
+
+  data.forEach((item) => {
     cities.push(item.city);
   });
-  
-  const [city, setCity] = useState<string>("Все города");
+
+  useEffect(() => {
+    if (city !== 'Все города') {
+      const result = data.filter((person) => person.city == city);
+      setProps(result);
+    } else {
+      setProps(data);
+    }
+  }, [city]);
 
   return (
     <section className={styles.main}>
       <div className={styles.header}>
         <DropdownList data={cities} state={city} setState={setCity} />
-        <Link to={"/maps"} className={styles.link}>
+        <Link to={'/maps'} className={styles.link}>
           Посмотреть на карте
         </Link>
       </div>
