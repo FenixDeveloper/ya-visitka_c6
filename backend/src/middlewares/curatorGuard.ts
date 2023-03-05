467c41180abf5cce2d32a7d3211b42cffdcb55b5
Mikhail Nenashev <nord-vizjerei@yandex.ru>
2023-03-05 19:36:43 +0200

    curatorGuard fix and removal of unused constants

--- backend/src/middlewares/curatorGuard.ts
+++ backend/src/middlewares/curatorGuard.ts
@@ -1,21 +1,16 @@
 import { NextFunction, Response, Request } from 'express';
-import { MSG_SERVER_ERROR, ROLE_CURATOR } from '../constants';
+import { ROLE_CURATOR } from '../constants';
 import ForbiddenError from '../errors/ForbiddenError';
-import InternalServerError from '../errors/InternalServerError';
 
 const curatorGuard = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    if (req.session.passport.user.role === ROLE_CURATOR) {
-      next();
-    } else {
-      next(new ForbiddenError());
-    }
-  } catch (err) {
-    next(new InternalServerError(MSG_SERVER_ERROR));
+  if (req.session.passport.user.role === ROLE_CURATOR) {
+    next();
+  } else {
+    next(new ForbiddenError());
   }
 };
 
